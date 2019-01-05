290a9f154e52c57a8aaa868221fe61e703099bda
Priscilla H <priscillamhuynh@gmail.com>
2019-01-05 10:34:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    hour = t.hour
+    hour = time.hour
     if hour < 12
     resp.write "Good Morning!"
     else
