65f4fdd3551a099cc7e048ef786519f0fa0702a7
Priscilla H <priscillamhuynh@gmail.com>
2019-01-05 10:30:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     time = Time.now
-    resp.write '#{time}'
+    resp.write time
     resp.write "Good Morning!"
     resp.write "Good Afternoon!"
     resp.finish
